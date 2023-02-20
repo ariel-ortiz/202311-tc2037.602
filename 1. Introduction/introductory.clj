f67c74a208c843e4b0d0e08c7163af456c2495ca
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-02-20 12:53:33 -0600

    Add solutions to problems 3 and 4 of problem set 1

--- 1. Introduction/introductory.clj
+++ 1. Introduction/introductory.clj
@@ -26,4 +26,28 @@
   (is (= 0.0 (fahrenheit->celsius 32.0)))
   (is (= -40.0 (fahrenheit->celsius -40.0))))
 
+(defn sign
+  [n]
+  (cond
+    (neg? n) -1
+    (pos? n) 1
+    :else  0))
+
+(deftest test-sign
+  (is (= -1 (sign -5)))
+  (is (= 1 (sign 10)))
+  (is (= 0 (sign 0))))
+
+(defn roots
+  [a b c]
+  (let [d (- b)
+        e (sqrt (- (* b b) (* 4 a c)))
+        f (* 2 a)]
+    [(/ (+ d e) f) (/ (- d e) f)]))
+
+(deftest test-roots
+  (is (= [-1 -1] (roots 2 4 2)))
+  (is (= [0 0] (roots 1 0 0)))
+  (is (= [-1/4 -1] (roots 4 5 1))))
+
 (run-tests)
