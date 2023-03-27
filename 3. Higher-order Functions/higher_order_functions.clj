45ed427bd47895a583c0c573b980a8d0887b85a2
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-03-27 12:57:09 -0600

    Resuelve problema 3 del problemario 4

--- 3. Higher-order Functions/higher_order_functions.clj
+++ 3. Higher-order Functions/higher_order_functions.clj
@@ -37,4 +37,41 @@
   (is (there-exists-one symbol?
                         '(4 8 15 sixteen 23 42))))
 
+;; Problem 3
+(defn linear-search
+  [vct x eq-fun]
+  (loop [i 0]
+    (cond
+      (= i (count vct)) nil
+      (eq-fun x (vct i)) i
+      :else (recur (inc i)))))
+
+(deftest test-linear-search
+  (is (nil? (linear-search [] 5 =)))
+  (is (= 0 (linear-search [5] 5 =)))
+  (is (= 4 (linear-search
+             [48 77 30 31 5 20 91 92
+              69 97 28 32 17 18 96]
+             5
+             =)))
+  (is (= 3 (linear-search
+             ["red" "blue" "green" "black" "white"]
+             "black"
+             identical?)))
+  (is (nil? (linear-search
+              [48 77 30 31 5 20 91 92
+               69 97 28 32 17 18 96]
+              96.0
+              =)))
+  (is (= 14 (linear-search
+              [48 77 30 31 5 20 91 92
+               69 97 28 32 17 18 96]
+              96.0
+              ==)))
+  (is (= 8 (linear-search
+             [48 77 30 31 5 20 91 92
+              69 97 28 32 17 18 96]
+             70
+             #(<= (abs (- %1 %2)) 1)))))
+
 (run-tests)
