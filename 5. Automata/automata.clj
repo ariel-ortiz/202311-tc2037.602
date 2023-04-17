7b4c30f8eaffa4605cfe028fe04e219e58d3f773
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-04-17 12:58:08 -0600

    Resuelve problema 1 del problemario 6

---
+++ 5. Automata/automata.clj
@@ -0,0 +1,39 @@
+(ns automata
+  (:require [clojure.test :refer [deftest is run-tests]]))
+
+(defrecord DFA [initial-state
+                accept-states
+                transitions])
+
+(def dfa-1 (->DFA :q0
+                  #{:q2}
+                  {:q0 {\a :q1
+                        \b :q0}
+                   :q1 {\a :q1
+                        \b :q2}
+                   :q2 {\a :q2
+                        \b :q2}}))
+
+(defn accepts?
+  [dfa input]
+  (let [initial-state (.initial-state dfa)
+        accept-states (.accept-states dfa)
+        transitions (.transitions dfa)]
+    (loop [input (seq input)
+           current initial-state]
+      (if (empty? input)
+        (contains? accept-states current)
+        (recur (rest input)
+               ((transitions current) (first input)))))))
+
+(deftest test-problem1
+  (is (accepts? dfa-1 "ab"))
+  (is (accepts? dfa-1 "abba"))
+  (is (accepts? dfa-1 "aaab"))
+  (is (accepts? dfa-1 "abbbbbbbbb"))
+  (is (not (accepts? dfa-1 "")))
+  (is (not (accepts? dfa-1 "a")))
+  (is (not (accepts? dfa-1 "baa")))
+  (is (not (accepts? dfa-1 "bbba"))))
+
+(run-tests)
