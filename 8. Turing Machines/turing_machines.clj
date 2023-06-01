70be91a87e779d35fb1523013ce7aed620800e20
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-06-01 13:28:23 -0600

    Resuelve problemas 1 y 2 del problemario 9

--- 8. Turing Machines/turing_machines.clj
+++ 8. Turing Machines/turing_machines.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [deftest is run-tests]])
   (:import (java.io Writer)))
 
-(defrecord TM [intial-state accept-states transitions])
+(defrecord TM [initial-state accept-states transitions])
 
 (defrecord Tape [left head right]
   Object
@@ -12,15 +12,11 @@
   [self ^Writer writer]
   (.write writer (str self)))
 
-; (->Tape "aaaa" \b "cccc")
-
 (defn make-tape
   ([s]
    (let [s (drop-while #(= % \_) s)]
      (make-tape ""
-                (if (= s "")
-                  \_
-                  (first s))
+                (if (empty? s) \_ (first s))
                 (rest s))))
   ([left head right]
    (let [left (drop-while #(= % \_) left)
@@ -29,24 +25,82 @@
              head
              (apply str right)))))
 
-; (make-tape "____h_o_l_a____")
-
 (defn write-tape
   [{:keys [left right]} value]
   (make-tape left value right))
 
-; (write-tape (make-tape "ho" \l "a") \x)
+(defn shift-head
+  [{:keys [left head right]} direction]
+  (case direction
+    :right (make-tape (str left head)
+                      (or (first right) \_)
+                      (rest right))
+    :left (make-tape (or (butlast left) ())
+                     (or (last left) \_)
+                     (str head right))
+    (throw (ex-info (str "Bad direction: " direction) {}))))
 
-;(defn shift-head
-;  [{:keys [left head right]} direction]
-;  (case direction
-;    :right (make-tape (str left head) (or (first right) \_) (rest right))
-;    :left (make-tape)
-;    (throw (ex-info (str "Bad direction: " direction) {}))))
+(defn accepts
+  [{:keys [initial-state accept-states transitions]} input]
+  (loop [tape (make-tape input)
+         current-state initial-state]
+    (if (contains? accept-states current-state)
+      (str tape)
+      (if-let [[write-symbol direction new-state]
+               ((transitions current-state) (.head tape))]
+        (recur (shift-head (write-tape tape write-symbol) direction)
+               new-state)
+        nil))))
 
+;----------------------------------------------------------
+; Problem 1
 
 (def tm-1 (->TM :q0
                 #{:q2}
                 {:q0 {\a [\a :right :q1]
                       \_ [\_ :left :q2]}
                  :q1 {\a [\a :right :q0]}}))
+
+; (accepts tm-1 "")
+
+(deftest test-problem1
+  (is (= "[_]"
+         (accepts tm-1 "")))
+  (is (= "a[a]"
+         (accepts tm-1 "aa")))
+  (is (= "aaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaa")))
+  (is (= "aaaaaaaaaaaaaaaaaaaaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaaa")))
+  (is (nil? (accepts tm-1 "a")))
+  (is (nil? (accepts tm-1 "aaa")))
+  (is (nil? (accepts tm-1 "aaaaaaa")))
+  (is (nil? (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaa"))))
+
+;----------------------------------------------------------
+; Problem 2
+
+(def tm-2 (->TM :q0
+                #{:q3}
+                {:q0 {\0 [\0 :right :q1]
+                      \1 [\1 :right :q2]}
+                 :q1 {\1 [\1 :right :q1]
+                      \_ [\_ :left :q3]}
+                 :q2 {\0 [\0 :right :q2]
+                      \_ [\_ :left :q3]}}))
+
+(deftest test-problem2
+  (is (= "[0]"
+         (accepts tm-2 "0")))
+  (is (= "[1]"
+         (accepts tm-2 "1")))
+  (is (= "1[0]"
+         (accepts tm-2 "10")))
+  (is (= "0111111111[1]"
+         (accepts tm-2 "01111111111")))
+  (is (nil? (accepts tm-2 "")))
+  (is (nil? (accepts tm-2 "00")))
+  (is (nil? (accepts tm-2 "100000000001")))
+  (is (nil? (accepts tm-2 "10011010100101011"))))
+
+(run-tests)
